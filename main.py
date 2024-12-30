cf3b989f0050b7673040908b725183661c2ac81e
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-30 23:02:18 +0530

    news modified

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import requests
 from send_email import send_email
 
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-11-30&sortBy=publishedAt&apiKey=")
+topic = "tesla"
+API_KEY = "####"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2024-11-30&"
+       "sortBy=publishedAt&"
+       f"apiKey={API_KEY}&"
+       "language=en")
 
 request = requests.get(url)
 content = request.json()
 
 raw_message = ""
-for article in content["articles"]:
-    article_message = f"{article['title']} \n {article['description']} \n\n"
+for article in content["articles"][:10]:
+    article_message = f"{article['title']} \n{article['description']} \n{article['url']} \n\n"
     raw_message = raw_message + article_message
 
 message = f"""\
 Subject: Daily News
 
 {raw_message}
+
+Thanks,
+goks
 """
 
 message = message.encode("utf-8")
-
 send_email(message)
-print(content)
+print("Done")
