903b608e0a1ee06c005bf1ce44cd53c888327fc3
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-30 17:27:27 +0530

    send email function added

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import requests
+from send_email import send_email
 
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-11-30&sortBy=publishedAt&apiKey=")
 
 request = requests.get(url)
 content = request.json()
+
+raw_message = ""
 for article in content["articles"]:
-    print(article["title"])
+    article_message = f"{article['title']} \n {article['description']} \n\n"
+    raw_message = raw_message + article_message
+
+message = f"""\
+Subject: Daily News
+
+{raw_message}
+"""
+
+message = message.encode("utf-8")
+
+send_email(message)
 print(content)
